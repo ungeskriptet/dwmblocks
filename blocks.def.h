e88d1b4cb8b7ef527740dfb9e288293482923749
David W <w.david0@protonmail.com>
2020-12-24 14:20:40 +0100

    Use netctl for Wi-Fi

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"󰤨 ", "nmcli -t -f name connection show --active",      5,      0},
+	{"󰤨 ", "netctl list |grep \"* wlan0-\" | sed -e 's/* wlan0-\\(.*\\)/\\1/'",      5,      0},
 	{"󰕾 ",  "[ $(pamixer --get-mute) = true ] && echo 0 && exit||pamixer --get-volume",0,10},
 	{"󱑍 ", "date +%a', '%T",                           1,      0},
 
